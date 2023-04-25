28899649cef4c7c2b6c95b9b5f208965a6f129e9
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-24 21:19:19 -0300

    Add files via upload

---
+++ CalculadoraCore/Calcu.cs
@@ -0,0 +1,36 @@
+﻿using Calc.core.Interfaces;
+
+namespace CalculadoraCore
+{
+    public class Calcu: Icalc
+    {
+        public EventHandler Procesando { get; set; }
+        public EventHandler Termino { get; set; }
+        public int Suma(int x, int y)
+        {
+            if (Procesando != null)
+            {
+                Procesando.Invoke(this, new EventArgs());
+            }
+            var resultado = x + y ;
+            Thread.Sleep(1000);
+            if (Termino != null)
+            {
+                Termino.Invoke(this, new EventArgs());
+            }
+            return resultado;
+        }
+        public int Resta(int x, int y)
+        {
+            return x - y;
+        }
+        public int Division(int x , int y)
+        {
+            return x / y;
+        }
+        public int Multiplicacion(int x , int y)
+        {
+            return x * y;
+        }
+    }
+}
