a346c330758439c433f3a2402d229a03a034efe0
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-24 21:18:12 -0300

    Add files via upload

---
+++ Cal.core.Interfaces/Icalc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.core.Interfaces;
+
+public interface Icalc
+{
+    public EventHandler Procesando { get; set; }
+    public EventHandler Termino { get; set; }
+    int Suma(int x, int y);
+    int Resta(int x, int y);
+    int Division(int x, int y);
+    int Multiplicacion(int x, int y);
+}
---
+++ CalcSuperPower/Calcsuperpower.cs
@@ -0,0 +1,41 @@
+﻿using Calc.core.Interfaces;
+
+namespace CalcSuperPower
+{
+    public class Calcsuperpower : Icalc
+    {
+        public EventHandler Procesando { get; set; }
+        public EventHandler Termino { get; set; }
+
+
+        public int Suma(int x, int y)
+        {
+            if (Procesando != null)
+            {
+                Procesando.Invoke(this, new EventArgs());
+            }
+            var resultado = x + y + 1000;
+            Thread.Sleep(1000);
+
+            if (Termino != null)
+            {
+                Termino.Invoke(this, new EventArgs());
+            }
+            return resultado;
+        }
+        public int Resta(int x, int y)
+        {
+            return x - y + 1000;
+        }
+        public int Division(int x, int y)
+        {
+            return x / y + 1000;
+
+        }
+        public int Multiplicacion(int x, int y)
+        {
+            return x * y + 1000;
+        }
+
+    }
+}
